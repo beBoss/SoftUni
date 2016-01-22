5ff255497b82b18314501ed501fff418e9410fe0
beBoss™ <beBoss.bg@gmail.com>
2016-01-22 23:01:22 +0000

    Added Homework: Defining Classes Added Problem 1. Persons

---
+++ Object-Oriented Programming/Object-Oriented Programming/Problem-1-Persons/Person.cs
@@ -0,0 +1,82 @@
+﻿namespace Problem_1_Persons
+{
+    using System;
+
+    public class Person
+    {
+        private string name;
+        private int age;
+        private string email;
+
+        public Person(string name, int age, string email)
+            : this(name, age)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Email = email;
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be empty!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value < 1 || value > 100)
+                {
+                    throw new ArgumentException("Age must be between 1 and 100!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || !value.Contains("@"))
+                {
+                    throw new ArgumentException("Email cannot be null, empty or not contains \"@\" ");
+                }
+
+                this.email = value;
+            }
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine("Name: {0}, Age: {1}, Email: {2}", 
+                this.name, this.age, this.email);
+        }
+    }
+}
---
+++ Object-Oriented Programming/Object-Oriented Programming/Problem-1-Persons/Persons.cs
@@ -0,0 +1,16 @@
+﻿namespace Problem_1_Persons
+{
+    public class Persons
+    {
+        public static void Main()
+        {
+            Person pesho = new Person("pesho", 20);
+            Person gosho = new Person("gosho", 100, "gosho@gosho.com");
+            Person ivan = new Person("ivan", 34, "ivan@ivan.com");
+
+            pesho.ToString();
+            gosho.ToString();
+            ivan.ToString();
+        }
+    }
+}
